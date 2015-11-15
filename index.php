8213c68659ea9f394b38fd46842f051a86a635d3
Belbola <mohamedakramzaytar@gmail.com>
2015-11-15 02:25:43 +0000

    we start, bitches

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
-<?php
-echo 'Hello World';
-?>
+<html>
+	<head>
+		<title>Busni</title>
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/css/theme.css">
+	</head>
+
+	<body>
+		<div class="container">
+	      <div class="header clearfix">
+	        <nav>
+	          <ul class="nav nav-pills pull-right">
+	            <li role="presentation" class="active"><a href="#">Home</a></li>
+	            <li role="presentation"><a href="#">Login</a></li>
+	          </ul>
+	        </nav>
+	        <h3 class="text-muted">Busni</h3>
+	      </div>
+
+	      <div>
+	      	We start here, Bitches !
+	      	<br>
+	      	<br>
+	      </div>
+
+	      <footer class="footer">
+	        <p>Busni &copy; 2015</p>
+	      </footer>
+
+	    </div> <!-- /container -->
+	</body>
+</html>
