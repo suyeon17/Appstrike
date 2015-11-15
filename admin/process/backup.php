543222c3ee94de06d7e77f3e729ba43cbd55fc66
Belbola <mohamedakramzaytar@gmail.com>
2015-11-15 21:44:45 +0000

    added backup mechanism

--- admin/process/backup.php
+++ admin/process/backup.php
@@ -1,5 +1,6 @@
 <?php
-	// zip the whole code and make it downlodable !
-	$output = shell_exec('XXXXXXXXXX');
-	fopen($output);
+$filename='database_backup_'.date('G_a_m_d_y').'.sql';
+$result=exec('mysqldump busni --password=19911991 --user=root --single-transaction >../uploads/'.$filename,$output);
+header("Location: http://localhost/Newspaper_management/admin/uploads/".$filename);
+die();
 ?>
