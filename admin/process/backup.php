d8bdbb524da3081a15315b435d3d7064e5f2702b
Belbola <mohamedakramzaytar@gmail.com>
2015-11-16 17:28:05 +0000

    ok

--- admin/process/backup.php
+++ admin/process/backup.php
@@ -1,4 +1,12 @@
 <?php
+session_start();
+if(!isset($_SESSION['login'])) {
+	// admin not online
+	header("Location: http://localhost/Newspaper_management/admin/login.php");
+	die();
+}
+
+<?php
 $filename='database_backup_'.date('G_a_m_d_y').'.sql';
 $result= exec('mysqldump busni --password=19911991 --user=root --single-transaction >../uploads/'.$filename,$output);
 header("Location: http://localhost/Newspaper_management/admin/uploads/".$filename);
